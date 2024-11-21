a6d101438afd3e4371af4a62af56715a9fbaa423
pumarts <lokeshtheking50@gmail.com>
2024-11-21 19:55:59 +0530

    Create 5b.c

---
+++ 5b.c
@@ -0,0 +1,23 @@
+public class IO
+{
+public static void main(String args[]) throws IOException, FileNotFoundException
+{
+char c;
+int i;
+FileInputStream fin = new FileInputStream("Test1.txt");
+FileOutputStream fout = new FileOutputStream("Test1.txt");
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+System.out.println("Enter characters, 'q' to quit.");
+do{
+c = (char) br.read();
+if(c != 'q') fout.write(c);
+} while(c != 'q');
+System.out.println("Contents of the file are");
+do{
+i = fin.read();
+if(i != -1) System.out.print((char) i);
+} while(i != -1);
+fin.close();
+fout.close();
+}
+}
